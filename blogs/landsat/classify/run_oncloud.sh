6de963ea0355ac2a44b396602c0cad5480ba27cf
Yongyao Jiang <J.yongyao11@gmail.com>
2018-07-06 16:51:50 -0400

    Updated model path

--- blogs/landsat/classify/run_oncloud.sh
+++ blogs/landsat/classify/run_oncloud.sh
@@ -21,7 +21,7 @@ python ./dfndvi.py \
     --autoscaling_algorithm=THROUGHPUT_BASED \
     --output_file=gs://$BUCKET/landsat-clf/output/scenes.txt \
     --output_dir=gs://$BUCKET/landsat-clf/output \
-    --model_dir=gs://eotest-207015-mlengine/model.joblib \
+    --model_dir=gs://$BUCKET/ml-model/model.joblib \
     --job_name=monthly-landsat-clf \
     --machine_type=n1-highmem-16 \
     --save_main_session \
